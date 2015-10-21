c8063ea46f9e80dd22d4a72c89fbc0e859183bb0
Antoine Riviere <aereivir@gmail.com>
2015-10-21 15:29:42 -0700

    game configuration file

---
+++ app/src/main/java/com/ar/minesweeper/game/GameConfiguration.java
@@ -0,0 +1,17 @@
+package com.ar.minesweeper.game;
+
+/**
+ * Created by ariviere on 21/10/15.
+ */
+public class GameConfiguration {
+
+    /**
+     * size of the board in squares
+     */
+    public static final int BOARD_SIZE = 8;
+
+    /**
+     * number of mines on the board
+     */
+    public static final int MINES_NUMBER = 10;
+}
